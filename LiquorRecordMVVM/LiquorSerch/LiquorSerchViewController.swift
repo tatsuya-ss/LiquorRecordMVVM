bb80ec39c3a041323d486fd668f098af3277046b
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-07-13 11:33:00 +0900

    LiquorSerch画面を追加しました

---
+++ LiquorRecordMVVM/LiquorSerch/LiquorSerchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LiquorSerchViewController.swift
+//  LiquorRecordMVVM
+//
+//  Created by 坂本龍哉 on 2021/07/13.
+//
+
+import UIKit
+
+class LiquorSerchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
